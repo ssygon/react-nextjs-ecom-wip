161855e50757d55d8ab38a1debcd3a8fda9de665
Shaun Ong <ssygon@gmail.com>
2024-09-11 14:35:59 +0800

    SearchBar styling

--- src/app/components/SearchBar/SearchBar.tsx
+++ src/app/components/SearchBar/SearchBar.tsx
@@ -1,7 +1,15 @@
+import IconSearch from "@/app/icons/search.svg";
+
+
 const SearchBar = () => {
   return (
     <>
-      <input type="text" placeholder="Search" />
+      <form className="flex items-center gap-2 bg-gray-100 rounded-md flex-1 max-w-[220px] py-2 px-3">
+        <input type="text" placeholder="Search" className="w-full flex-1 bg-transparent outline-none" />
+        <button className="cursor-pointer">
+          <IconSearch width={20} height={20} viewBox={"0 0 56 56"}/>
+        </button>
+      </form>
     </>
   );
 };
