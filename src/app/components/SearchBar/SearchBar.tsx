3470dae9c55f4d3985cc7b0fd545c8e6224c322c
Shaun Ong <ssygon@gmail.com>
2024-09-11 14:47:05 +0800

    SearchBar functionality

--- src/app/components/SearchBar/SearchBar.tsx
+++ src/app/components/SearchBar/SearchBar.tsx
@@ -1,11 +1,29 @@
+"use client";
+
 import IconSearch from "@/app/icons/search.svg";
+import { useRouter } from "next/navigation";
 
 
 const SearchBar = () => {
+
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const searchInput = formData.get("search") as string;
+    console.log(searchInput);
+
+    if (searchInput) {
+      router.push(`/list?search=${searchInput}`);
+    }
+  };
+
   return (
     <>
-      <form className="flex items-center gap-2 bg-gray-100 rounded-md flex-1 max-w-[220px] py-2 px-3">
-        <input type="text" placeholder="Search" className="w-full flex-1 bg-transparent outline-none" />
+      <form className="flex items-center gap-2 bg-gray-100 rounded-md flex-1 max-w-[220px] py-2 px-3" onSubmit={handleSearch}>
+        <input name="search" type="text" placeholder="Search" className="w-full flex-1 bg-transparent outline-none" />
         <button className="cursor-pointer">
           <IconSearch width={20} height={20} viewBox={"0 0 56 56"}/>
         </button>
